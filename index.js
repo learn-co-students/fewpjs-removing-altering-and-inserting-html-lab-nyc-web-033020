4b428d9a849d083889c25ae404c53dd93a8ff03e
dkersten <dkersten1994@gmail.com>
2020-05-09 17:13:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+let main = document.querySelector("main#main");
+main.remove();
+
+let newHeader = document.createElement('H1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Dan is the champion!";
