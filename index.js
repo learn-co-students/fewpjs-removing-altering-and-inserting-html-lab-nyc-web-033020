327bc6f616ac4e49980b724f98ec829175fb1cdb
theluispestana <theluispestana@gmail.com>
2020-05-10 16:42:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.body.removeChild(document.getElementById('main'));
+
+let newHeader = document.body.appendChild(document.createElement('h1'));
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Luis is the champion'
