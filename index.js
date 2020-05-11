7a2c217ffb10401d378d6941071f2526b4657bfd
William <willlin1996@gmail.com>
2020-05-11 17:47:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+document.querySelector("main#main").remove();
+// find the main with id main
+const newHeader = document.createElement("h1");
+// make an h1 in the document
+newHeader.id = "victory"
+// the id of the h1 to be "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+// the inner html is now this
+newHeader.className = "victory";
+// the class within this is victory
