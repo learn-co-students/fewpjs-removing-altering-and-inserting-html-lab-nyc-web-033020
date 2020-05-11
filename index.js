4c5f9695a752d40a56d9c2e5a3021cb0e2642e06
Edgar <edgar.a.rivera08@gmail.com>
2020-05-10 22:58:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const testVar = "hi"
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML("Edgar is the champion" );
+
